94b97f2b65989dda2b247305444fdd0bc29915d1
dianmei <94010846@qq.com>
2016-12-11 21:36:30 +0800

    change 2

--- microservice-config-server/src/main/java/com/itmuch/cloud/dianmei/ConfigServerApplication.java
+++ microservice-config-server/src/main/java/com/itmuch/cloud/dianmei/ConfigServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 /**
  * 通过@EnableConfigServer注解激活配置服务.
  * 说明：
- * 在application.yml中有个git.uri的配置，目前配置的是https://github.com/eacdy/spring-cloud-study/
+ * 在application.yml中有个git.uri的配置，目前配置的是https://github.com/dianmei/dianmei_config/
  * 获取git上的资源信息遵循如下规则：
  * /{application}/{profile}[/{label}]
  * /{application}-{profile}.yml
