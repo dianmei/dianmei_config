8e52451c67a6b27c901b96cb2beb377bd9aa457e
dianmei <94010846@qq.com>
2016-12-11 20:58:24 +0800

    First commit

---
+++ microservice-provider-user/src/main/java/com/itmuch/cloud/dianmei/domain/User.java
@@ -0,0 +1,52 @@
+package com.itmuch.cloud.dianmei.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  @Column
+  private String username;
+  @Column
+  private Integer age;
+
+	public User() {
+		super();
+	}
+	public User(Long id, String username, Integer age) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.age = age;
+	}
+
+public Long getId() {
+    return this.id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return this.username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public Integer getAge() {
+    return this.age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+}
