91e0100a580a03ae744f28d2a073bbd2de0be768
dianmei <94010846@qq.com>
2016-12-18 23:09:09 +0800

    3

--- microservice-api-gateway/src/main/java/com/itmuch/cloud/dianmei/ZuulApiGatewayApplication.java
+++ microservice-api-gateway/src/main/java/com/itmuch/cloud/dianmei/ZuulApiGatewayApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.amazonaws.auth.policy.Principal;
 
 /**
- * 使用@EnableZuulProxy注解激活zuul。
+ * 使用@EnableZuulProxy注解zuul。
  * 跟进该注解可以看到该注解整合了@EnableCircuitBreaker、@EnableDiscoveryClient，是个组合注解，目的是简化配置。
  * @author linyue
  */
